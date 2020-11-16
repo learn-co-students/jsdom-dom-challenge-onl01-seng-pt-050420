5bb2286942ddb6be37bb8d90dbf7b19c16028cd5
danielkwon89 <danielkwon89@gmail.com>
2020-11-15 23:22:31 -0500

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,3 +1,5 @@
+// comment function
+
 let commentList = document.getElementById('list');
 
 let commentForm = document.getElementById('comment-form');
@@ -9,9 +11,49 @@ commentForm.addEventListener('submit', (e) => {
 });
 
 function addComment(){
-    let li = document.createElement('li');
+    let p = document.createElement('p');
     let comment = commentForm.elements['comment-input'].value;
     let node = document.createTextNode(comment);
-    li.appendChild(node);
-    commentList.appendChild(li);
+    p.appendChild(node);
+    commentList.appendChild(p);
 };
+
+
+// like button function
+
+let counter = document.getElementById('counter').innerText;
+
+let likeButton = document.getElementById('heart');
+
+let likeList = document.getElementsByClassName('likes')[0];
+
+likeButton.addEventListener('click', (e) =>{
+    if (document.getElementById(counter)){
+        addLike();
+    } else {
+        newLike();
+    };
+});
+
+function newLike(){
+    let li = document.createElement('li');
+    li.setAttribute('id', counter);
+    let span = document.createElement('span');
+    span.innerText = 1;
+    li.appendChild(span);
+    li.innerHTML = li.id + " has been liked " + li.innerHTML + " time";
+    likeList.appendChild(li);
+};
+
+function addLike(){
+    let like = document.getElementById(counter);
+    let element = like.children[0];
+    element.innerText = parseInt(element.innerText) + 1;
+    if (element.innerText == 2){
+        like.innerHTML = like.innerHTML + "s";
+    };
+};
+
+
+// timer adds +1 to counter per second
+
